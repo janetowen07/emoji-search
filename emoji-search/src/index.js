f64557b13d07b34625856c557fe24360c2d3aae6
Janet <janetowen07@gmail.com>
2017-09-09 22:25:40 +0100

    link emoji list to index.js file

--- emoji-search/src/index.js
+++ emoji-search/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import emojis from './emojiList.json';
+
+ReactDOM.render(<App emojis={emojis}/>, document.getElementById('root'));
 registerServiceWorker();
