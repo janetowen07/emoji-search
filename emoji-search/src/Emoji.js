7530d926f1a0266719b5ad06de1a8eeb0349357a
Janet <janetowen07@gmail.com>
2017-09-09 22:32:50 +0100

    emoji component

---
+++ emoji-search/src/Emoji.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Emoji extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>{this.props.symbol} { this.props.title} </p>
+			</div>
+		);
+	}
+}
+
+export default Emoji;
